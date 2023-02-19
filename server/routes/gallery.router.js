a8952a5b64732d202ee174d4e0fd2d2ca95cf476
Andrea Love <andrealovelovelove@gmail.com>
2023-02-19 16:12:57 -0600

    added put request logic in server and installed pg

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+    const sqlText = `
+        UPDATE "gallery"
+        SET "likes" = "likes" + 1
+        WHERE id =$1;
+        `
+    const sqlParams =  [req.params.id]
+    pool.query(sqlText, sqlParams)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log("error in server PUT:", err);
+    });
+});
+     // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    const sqlText = `SELECT * FROM gallery ORDER BY id;` 
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`Got stuff from the database`, result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+  
 }); // END GET Route
 
 module.exports = router;

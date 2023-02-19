b5cf233ebc18e4b18a5782e20c8de1a6e92a0b17
Andrea Love <andrealovelovelove@gmail.com>
2023-02-18 23:36:16 -0600

    added margin for page body

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -6,7 +6,7 @@ import Masonry from '@mui/lab/Masonry';
 
 function GalleryList({getItems, galleryList}){
     return (
-        <Box sx={{ width: 800, height: 600, overflowY: 'scroll' }}>
+        <Box sx={{ width: 800, height: 800, overflowY: 'auto', mx: 'auto' }}>
             <Masonry columns={4} spacing={2}>
                 {galleryList.map((galleryItem) => {
                 return <GalleryItem key={galleryItem.id} getItems={getItems} galleryItem={galleryItem}/>
